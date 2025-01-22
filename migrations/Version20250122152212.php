a5f9e8018aa7192e6edf538faf66f881d5b0378f
DaxaXAd <g1thirha@outlook.com>
2025-01-22 16:28:21 +0100

    migration forced

--- migrations/Version20250122152212.php
+++ migrations/Version20250122152212.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250122151243 extends AbstractMigration
+final class Version20250122152212 extends AbstractMigration
 {
     public function getDescription(): string
     {

db9dbdac3bcb356807608d7da6f304bed56e5986
DaxaXAd <g1thirha@outlook.com>
2025-01-24 12:21:13 +0100

    modif pwd feature for profilepage

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -10,6 +10,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
 
 class UserType extends AbstractType
 {
@@ -28,9 +29,21 @@ class UserType extends AbstractType
 
             if ($this->security->getUser()) {
                 $builder->add('password', PasswordType::class, [
+                    'required' => false,
+                    'empty_data' => '',
+                    'mapped' => false,
+                    'attr' => [
+                        'placeholder' => "Leave empty if you don't want to change it",
+                    ],
                     'constraints' => [
                         new NotBlank([
                             'message' => 'Please enter a password',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
                         ]),
                     ],
                 ])

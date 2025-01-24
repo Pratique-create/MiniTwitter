05071ed895fa2941437b7f3819776ef48dce7e3c
DaxaXAd <g1thirha@outlook.com>
2025-01-24 11:32:12 +0100

    added condition for password modification phase 1

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -34,7 +34,7 @@ class RegistrationController extends AbstractController
             // do anything else you need here, like send an email
 
             // return $security->login($user, 'form_login', 'main');
-            return $this->redirectToRoute('home_page');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,21 +3,41 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security as SecurityBundleSecurity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
+    private $security;
+
+    public function __construct(SecurityBundleSecurity $security)
+    {
+        $this->security = $security;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('email')
             ->add('number')
-            ->add('username')
+            ->add('username');
+
+            if ($this->security->getUser()) {
+                $builder->add('password', PasswordType::class, [
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Please enter a password',
+                        ]),
+                    ],
+                ])
             ->add('password')
             ->add('profilePicture')
         ;
+    }
     }
 
     public function configureOptions(OptionsResolver $resolver): void

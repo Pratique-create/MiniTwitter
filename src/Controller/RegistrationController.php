378e838ae00e6d089269ae61b75543c11ef9c6a7
6-il2 <sirine.4329@gmail.com>
2025-01-28 13:33:25 +0100

    update

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -34,7 +34,7 @@ class RegistrationController extends AbstractController
             // do anything else you need here, like send an email
             $security->login($user);
 
-            return $this->redirectToRoute('home_page');
+            return $this->redirectToRoute('app_posts_index');
         }
 
         return $this->render('registration/register.html.twig', [
@@ -43,46 +43,3 @@ class RegistrationController extends AbstractController
     }
 }
 
-
-// namespace App\Controller;
-
-// use App\Entity\User;
-// use App\Form\RegistrationFormType;
-// use Doctrine\ORM\EntityManagerInterface;
-// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Bundle\SecurityBundle\Security;
-// use Symfony\Component\HttpFoundation\Request;
-// use Symfony\Component\HttpFoundation\Response;
-// use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-// use Symfony\Component\Routing\Attribute\Route;
-
-// class RegistrationController extends AbstractController
-// {
-//     #[Route('/register', name: 'app_register')]
-//     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
-//     {
-//         $user = new User();
-//         $form = $this->createForm(RegistrationFormType::class, $user);
-//         $form->handleRequest($request);
-
-//         if ($form->isSubmitted() && $form->isValid()) {
-//             /** @var string $plainPassword */
-//             $plainPassword = $form->get('plainPassword')->getData();
-
-//             // encode the plain password
-//             $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
-
-//             $entityManager->persist($user);
-//             $entityManager->flush();
-
-//             // do anything else you need here, like send an email
-
-//             // return $security->login($user, 'form_login', 'main');
-//             return $this->redirectToRoute('app_posts_index');
-//         }
-
-//         return $this->render('registration/register.html.twig', [
-//             'registrationForm' => $form->createView(),
-//         ]);
-//     }
-// }

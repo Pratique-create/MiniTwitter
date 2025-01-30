983bc064b8da85884d4fd2790c6ebeb212f77e03
Saugeot <quentinsaugeot.arinfo@gmail.com>
2025-01-30 13:42:58 +0100

    Ajout bouton like et RT sur la page utilisateur

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -77,11 +77,12 @@ final class CommentController extends AbstractController
     #[Route('/{id}', name: 'app_comment_delete', methods: ['POST'])]
     public function delete(Request $request, Comment $comment, EntityManagerInterface $entityManager): Response
     {
+
         if ($this->isCsrfTokenValid('delete'.$comment->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($comment);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_comment_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_posts_show', ['id' => $comment->getPost()->getId()], Response::HTTP_SEE_OTHER);
     }
 }
